93cd3dbe68d50e22eb198e5b2d9ebb18427092d0
olychkam <olkaaamartynova@gmail.com>
2021-05-01 23:08:54 +0300

    useEffect (ProfileStatus)

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './ProfileInfo.module.css';
 
 type ProfileStatusType = {
@@ -10,6 +10,10 @@ type ProfileStatusType = {
 export const ProfileStatusWithHooks = (props: ProfileStatusType) => {
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+    useEffect(()=>{
+        setStatus(props.status)
+    },[props.status])
+
     const activateEditMode = () => {
         setEditMode(true)
     }
